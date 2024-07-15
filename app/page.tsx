1edf67501571fd58a2c1eb664ab39db6e8e81af3
M. Shafiq Sofian <muhdshafiqsofian@gmail.com>
2024-07-15 17:54:54 +0800

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Hello Next.js with Pico CSS!</h1>
+    </main>
+  );
+}
